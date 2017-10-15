71b7c1f5e9e0cbee47cc9a5c58cadb3271d0ab28
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 14:19:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  words = sentence.split(" ") # => ["Hi", "there", "Ruby"]
-  reversed_words = []
-  words.each do |word|
-    reversed_words << word.reverse!
-  end
-  reversed_words.join(" ")
-end
+# def reverse_each_word(sentence)
+#   words = sentence.split(" ") # => ["Hi", "there", "Ruby"]
+#   reversed_words = []
+#   words.each do |word|
+#     reversed_words << word.reverse!
+#   end
+#   reversed_words.join(" ")
+# end
 
 def reverse_each_word(sentence)
   words = sentence.split(" ") # => ["Hi", "there", "Ruby"]
